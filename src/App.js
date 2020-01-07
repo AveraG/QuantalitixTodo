5847dca27376d5f6bf88ec828ad1f070ed200af6
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-01-07 13:48:12 -0600

    cat picture, initial css, initial todo

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Feed dog',
+      isComlpleted: true,
+    },
+    {
+      content: 'Do dishes',
+      isCompleted: false,
+    },
+    {
+      content: 'Complete project',
+      isCompleted: false,
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="header">
+        <img src={require("./pictures/cat-checklist.jpg")} className="logo" alt="cat writing" />
+      </div>
+      <form className="todo-list">
+        <ul>
+          {todos.map((todo, i) => (
+            <div className="todo">
+              <div className="checkbox" />
+              <input type="text" value={todo.content} />
+            </div>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
+
+export default App;
