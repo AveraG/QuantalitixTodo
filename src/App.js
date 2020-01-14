69a844a84451dc55f27689c4cf342d3238b8abd4
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-01-14 13:30:33 -0600

    final tweaks

--- src/App.js
+++ src/App.js
@@ -20,11 +20,23 @@ function App() {
 
   function handleKeyDown(e, i) {
     if (e.key === 'Enter') {
-      createTodoAtIndex(e, i);
+      createTodo(e, i);
+    }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodo(i);
     }
   }
 
-  function createTodoAtIndex(e, i) {
+  function removeTodo(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function createTodo(e, i) {
     const newTodos = [...todos];
     newTodos.splice(i + 1, 0, {
       content: '',
@@ -36,6 +48,18 @@ function App() {
     }, 0);
   }
 
+  function updateTodo(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
+  function toggle(i) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[i].isComlpleted=!temporaryTodos[i].isComlpleted;
+    setTodos(temporaryTodos);
+  }
+
   return (
     <div className="app">
       <div className="header">
@@ -44,12 +68,17 @@ function App() {
       <form className="todo-list">
         <ul>
           {todos.map((todo, i) => (
-            <div className="todo">
-              <div className="checkbox" />
-              <input 
-              type="text" 
-              value={todo.content}
-              onKeyDown={e => handleKeyDown(e, i)} 
+            <div className={`todo ${todo.isComlpleted && 'todo-is-completed'}`}>
+              <div className={'checkbox'} onClick={() => toggle(i)}>
+                {todo.isCompleted && (
+                  <span>&#x2714;</span>
+                )}
+              </div>
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, i)}
+                onChange={e => updateTodo(e, i)}
               />
             </div>
           ))}
